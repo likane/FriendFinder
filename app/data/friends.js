05ba95480d8d05a1dca8f188b0689405f72870d9
Louis Kane <louiskane@1234.local>
2017-01-10 00:12:44 -0800

    added api info

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendArray = [
+	{
+		friendName: "joe",
+		friendPic: "http://stock-image.mediafocus.com/images/previews/word-joe-on-billboard-rs11334411785.jpg"
+		friendScore: [5,1,4,4,5,1,2,5,4,1]
+	}
+];
+
+module.exports = friendArray;
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-require("./app/routing/api-routes.js")(app);
-require("./app/routing/html-routes.js")(app);
+require("/app/routing/api-routes.js")(app);
+require("/app/routing/html-routes.js")(app);
 
 app.listen(PORT,function(){
 	console.log("APP LISTENING ON port: " PORT);
